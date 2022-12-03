5efa4b4b309205c2ae37fa0caaadeebdfd636e90
Leroy Mwanzia <leroykabs@gmail.com>
2022-12-03 23:51:19 +0300

    generate code files for diseases and pathogens

--- main.py
+++ main.py
@@ -1,44 +1,75 @@
-#%% Load libraries
+# %% Load libraries
 import pandas as pd
 
 
-def create_disease_line(disease):
+def create_code_text_line(data, element):
     # {"label": "DISEASE", "pattern": [{"lower": "cassava"}, {"lower": "scale"}]}
-    word_out = ''
-    for word in disease.split():
+
+    word_out: str = ''
+    output: str = ''
+    for word in data.split():
         if len(word_out) == 0:
             word_out = word_out + '{"lower":"' + word + '"}'
         else:
             word_out = word_out + ',{"lower":"' + word + '"}'
-    output = '{"label":"DISEASE","pattern":[' + word_out + ']}'
+
+    if element == 'disease':
+        output = '{"label":"DISEASE","pattern":[' + word_out + ']}'
+    elif element == 'pathogen':
+        output = '{"label":"PATHOGEN","pattern":[' + word_out + ']}'
+
     return output
 
 
-print(create_disease_line("Pseudocercospora leaf spot"))
-
-
-#%% Load data
+# %% Load data
 sweetpotato_df = pd.read_csv("data/Patterns-raw-sweet-potato.csv")
 potato_df = pd.read_csv("data/Patterns-raw-potato.csv")
 musa_df = pd.read_csv("data/Patterns-raw-Musa.csv")
 
+# %% view data
+# sweetpotato_df.info()
+# potato_df.info()
+# musa_df.info()
 
-#%% view data
-sweetpotato_df.info()
-potato_df.info()
-musa_df.info()
+# %% process data
 
-#%% process data
-# musa_disease = musa_df['Disease']
+musa_disease_code = musa_df['Disease'].dropna().apply(
+    lambda cropdf: create_code_text_line(cropdf, 'disease'))
+potato_disease_code = potato_df['Disease'].dropna().apply(
+    lambda cropdf: create_code_text_line(cropdf, 'disease'))
+sweetpotato_disease_code = sweetpotato_df['Disease'].dropna().apply(
+    lambda cropdf: create_code_text_line(cropdf, 'disease'))
+musa_pathogen_code = musa_df['Pathogen'].dropna().apply(
+    lambda cropdf: create_code_text_line(cropdf, 'pathogen'))
+potato_pathogen_code = potato_df['Pathogen'].dropna().apply(
+    lambda cropdf: create_code_text_line(cropdf, 'pathogen'))
+sweetpotato_pathogen_code = sweetpotato_df['Pathogen'].dropna().apply(
+    lambda cropdf: create_code_text_line(cropdf, 'pathogen'))
 
-# for disease in musa_df['Disease'].items():
-#     # if not disease.isnull():
-#     #     print(disease)
-#     print(type(disease))
-#     print(disease)
+# %% create back the code
 
-musa_diseases = musa_df['Disease'].dropna()
-print(musa_diseases)
-
-#%% learn some string stuff
-
+with open("data/output.jsonl", "w+") as f:
+    # sweetpotato pathogens
+    for lines in sweetpotato_pathogen_code:
+        f.write(lines)
+        f.write('\n')
+    # sweetpotato diseases
+    for lines in sweetpotato_disease_code:
+        f.write(lines)
+        f.write('\n')
+    # Potato pathogens
+    for lines in potato_pathogen_code:
+        f.write(lines)
+        f.write('\n')
+    # Potato diseases
+    for lines in potato_disease_code:
+        f.write(lines)
+        f.write('\n')
+    # Musa pathogens
+    for lines in musa_pathogen_code:
+        f.write(lines)
+        f.write('\n')
+    # Musa diseases
+    for lines in musa_disease_code:
+        f.write(lines)
+        f.write('\n')
