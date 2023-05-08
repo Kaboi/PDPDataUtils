bf15f3aa7765868965926defdedbc6eb9a0415b1
Leroy Mwanzia <leroykabs@gmail.com>
2023-05-08 13:35:14 +0300

    convert offset to iob

--- data_acquisition/convert_offset_iob.py
+++ data_acquisition/convert_offset_iob.py
@@ -9,54 +9,55 @@ out_file = "test-mine-output-iob-tags.tsv"
 
 
 # Function to convert a single text and its spans to IOB format
-def convert_to_iob(text, spans):
-    doc = nlp(text)
-    iob_tags = []
+def convert_to_iob(lnlp, txt, spns):
+    doc = lnlp(txt)
+    iob_tgs = []
 
-    for token in doc:
-        tag = "O"
+    for tkn in doc:
+        tg = "O"
 
-        for span in spans:
-            if token.idx == span["start"]:
-                tag = f"B-{span['label']}"
+        for span in spns:
+            if tkn.idx == span["start"]:
+                tg = f"B-{span['label']}"
                 break
-            elif token.idx > span["start"] and token.idx + len(token.text) <= span["end"]:
-                tag = f"I-{span['label']}"
+            elif tkn.idx > span["start"] and tkn.idx + len(tkn.text) <= span["end"]:
+                tg = f"I-{span['label']}"
                 break
 
-        iob_tags.append((token.text, tag))
+        iob_tgs.append((tkn.text, tg))
 
-    return iob_tags
+    return iob_tgs
+
 
 # Function to convert a list of texts and their spans to IOB format
-def adjust_annotation_spans(new_sentence_start, annotations):
+def adjust_annotation_spans(new_sentence_start, anns):
     adjusted_annotations = []
-    for annotation in annotations:
-        start = annotation["start"] - new_sentence_start
-        end = annotation["end"] - new_sentence_start
-        adjusted_annotations.append({"start": start, "end": end, "label": annotation["label"]})
+    for ann in anns:
+        start = ann["start"] - new_sentence_start
+        end = ann["end"] - new_sentence_start
+        adjusted_annotations.append({"start": start, "end": end, "label": ann["label"]})
     return adjusted_annotations
 
 
-#Function to split a list of texts and their spans into sentences
-def split_sentences(nlp, prodigy_annotations, min_length):
-    new_annotations = []
-    for annotation in prodigy_annotations:
-        text = annotation["text"]
-        spans = annotation["spans"]
-        doc = nlp(text)
+# Function to split a list of texts and their spans into sentences
+def split_sentences(nlp, anns, min_length):
+    new_anns = []
+    for ann in anns:
+        txt = ann["text"]
+        spns = ann["spans"]
+        doc = nlp(txt)
         start = 0
         for sent in doc.sents:
             if sent.end - start >= min_length:
                 new_text = doc[start:sent.end].text
-                new_spans = adjust_annotation_spans(doc[start].idx, spans)
-                new_annotations.append({"text": new_text, "spans": new_spans})
+                new_spans = adjust_annotation_spans(doc[start].idx, spns)
+                new_anns.append({"text": new_text, "spans": new_spans})
                 start = sent.end
         if start < len(doc):
             new_text = doc[start:].text
-            new_spans = adjust_annotation_spans(doc[start].idx, spans)
-            new_annotations.append({"text": new_text, "spans": new_spans})
-    return new_annotations
+            new_spans = adjust_annotation_spans(doc[start].idx, spns)
+            new_anns.append({"text": new_text, "spans": new_spans})
+    return new_anns
 
 
 # Define the input file path
@@ -80,14 +81,12 @@ with open(file_output_path, "w") as output_file:
     for annotation in new_annotations:
         text = annotation["text"]
         spans = annotation["spans"]
-        iob_tags = convert_to_iob(text, spans)
+        iob_tags = convert_to_iob(nlp, text, spans)
 
         # Write the IOB tags to the output file
         for token, tag in iob_tags:
             output_file.write(f"{token}\t{tag}\n")
         output_file.write("\n")
-
-
 
 
 # Load the JSONL annotations file
