6549e8b67bf0683a95297eeebb937c8d6bbca63f
Leroy Mwanzia <leroykabs@gmail.com>
2023-06-16 15:26:39 +0300

    fixed bugs with edge cases of multiple newlines in text.Added metadata file with stats

--- data_acquisition/convert_offset_iob.py
+++ data_acquisition/convert_offset_iob.py
@@ -2,6 +2,7 @@ import argparse
 import json
 import os
 import spacy
+from tqdm import tqdm
 
 
 def generate_output_filename(in_file_path):
@@ -10,7 +11,14 @@ def generate_output_filename(in_file_path):
     return os.path.join(dir_path, file_name + "-output-iob-tags.tsv")
 
 
-def convert_to_iob(lnlp, txt, spns):
+# TODO combine with generate_output_filename
+def generate_meta_filename(in_file_path):
+    file_name = os.path.splitext(os.path.basename(in_file_path))[0]
+    dir_path = os.path.dirname(in_file_path)
+    return os.path.join(dir_path, file_name + "-output-iob-tags-meta.txt")
+
+
+def convert_to_iob(lnlp, txt, spns, doc_hash=None):
     doc = lnlp(txt)
     iob_tgs = []
 
@@ -23,6 +31,19 @@ def convert_to_iob(lnlp, txt, spns):
             elif tkn.idx > span["start"] and tkn.idx + len(tkn.text) <= span["end"]:
                 tg = f"I-{span['label']}"
                 break
+
+        # Edge cases if not well preprocessed. If token is a newline character, assign 'O' tag
+        # This increase sequence/chunk size
+        # TODO - add a logger to log these edge cases for now print an warning message
+        if tkn.text.strip() == '' and not tg:
+            # Log a warning if multiple newline characters are found
+            if tkn.text.count("\n") > 1:
+                tqdm.write(f"Found multiple newline characters in _input_hash {doc_hash} text at position {tkn.idx}.")
+
+            # Add each newline character to iob_tgs with an 'O' tag
+            for newline in tkn.text:
+                iob_tgs.append(('', 'O'))
+            continue
 
         if not tg:
             tg = "O"
@@ -67,26 +88,47 @@ def load_annotations(in_file_path):
     return anns
 
 
-def convert_to_iob_save(lnlp, anns, out_file_path):
-    max_token_length = 0
+def convert_to_iob_save(lnlp, anns, in_file_path, out_file_path, meta_file_path):
+    max_text_sequence_length = 0
+    total_number_of_tokens = 0
+    total_number_of_annotated_documents = 0
+
     with open(out_file_path, "w") as output_file:
-        for annotation in anns:
-            text = annotation["text"]
-            spans = annotation["spans"]
-            iob_tags = convert_to_iob(lnlp, text, spans)
-            for token, tag in iob_tags:
-                output_file.write(f"{token}\t{tag}\n")
-            output_file.write("\n")
-            if len(iob_tags) > max_token_length:
-                max_token_length = len(iob_tags)
-    return max_token_length
+        for annotation in tqdm(anns, desc="Processing annotations"):
+            if annotation["answer"] == "accept":
+                total_number_of_annotated_documents += 1
+                text = annotation["text"]
+                spans = annotation["spans"]
+                input_hash = annotation["_input_hash"]
+                iob_tags = convert_to_iob(lnlp, text, spans, input_hash)
+                for token, tag in iob_tags:
+                    output_file.write(f"{token}\t{tag}\n")
+                    total_number_of_tokens += 1
+                output_file.write("\n")
+                if len(iob_tags) > max_text_sequence_length:
+                    max_text_sequence_length = len(iob_tags)
+            else:
+                # TODO - add a logger to log exclusion of reject and ignore print an warning message
+                tqdm.write(f"Excluding annotation with _input_hash {annotation['_input_hash']} due to answer: "
+                           f"{annotation['answer']}")
+
+    # Write the metadata to a file
+    with open(meta_file_path, "w") as meta_file:
+        meta_file.write(f"Input file: {in_file_path}\n")
+        meta_file.write(f"Output file: {out_file_path}\n")
+        meta_file.write(f"Total number of annotation documents: {total_number_of_annotated_documents}\n")
+        meta_file.write(f"Total number of tokens: {total_number_of_tokens}\n")
+        meta_file.write(f"Longest sequence of tokens: {max_text_sequence_length}\n")
 
 
 def main(file_path, language_model="en_core_web_lg", min_length=None):
     if not os.path.exists(file_path):
         print(f"Error: File {file_path} does not exist.")
         return
+
     file_output_path = generate_output_filename(file_path)
+    file_meta_path = generate_meta_filename(file_path)
+
     annotations = load_annotations(file_path)
     nlp = spacy.load(language_model, disable=["parser", "ner", "textcat"])
 
@@ -96,13 +138,13 @@ def main(file_path, language_model="en_core_web_lg", min_length=None):
 
         annotations = split_sentences(nlp, annotations, min_length=min_length)
 
-    longest_chunk = convert_to_iob_save(nlp, annotations, file_output_path)
-    print(f"Longest sequence: {longest_chunk}")
+    # TODO - Do this more elegantly
+    convert_to_iob_save(nlp, annotations, file_path, file_output_path, file_meta_path)
 
 
 # # %% Testing
 # # Edit paths here
-# input_file_path = "/home/leroy/Dev/Code/PDPAnnotationDataUtils/annotated_data/tests-mine.jsonl"
+# input_file_path = "/home/leroy/Dev/Code/PDPAnnotationDataUtils/annotated_data/tests-mine_2.jsonl"
 # # in_file = "ciat_ner_v2_combined_rev_20230106.jsonl"
 #
 # # Set the model to use or default to spacy
@@ -117,8 +159,8 @@ def main(file_path, language_model="en_core_web_lg", min_length=None):
 # main(input_file_path)
 # exit()
 
-# %% Main
 
+# %% Main
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Convert jsonl annotations to IOB format.')
     parser.add_argument('-f', '--file_path', required=True, help='Input file name with path.')
@@ -128,6 +170,3 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     main(args.file_path, args.spacy_model, args.min_length)
-
-
-
