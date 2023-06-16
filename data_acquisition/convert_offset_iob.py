1e4e04cdc858ae453a3b493cd682abf1571d5919
Leroy Mwanzia <leroykabs@gmail.com>
2023-06-16 10:31:25 +0300

    updates to converting from offset to IOB - Define minimum length, or print whole document.

--- data_acquisition/convert_offset_iob.py
+++ data_acquisition/convert_offset_iob.py
@@ -1,46 +1,21 @@
-# %% imports
+import argparse
 import json
 import os
 import spacy
-from transformers import BertTokenizerFast, RobertaTokenizerFast, AutoTokenizer
-
-# %% variables
-
-# Edit paths here
-file_path = "/home/leroy/Dev/Code/PDPAnnotationDataUtils/annotated_data"
-in_file = "tests-mine.jsonl"
-# in_file = "ciat_ner_v2_combined_rev_20230106.jsonl"
-
-# Set the model to use or default to spacy
-# Choose from "bert-base-uncased", "roberta-base", "allenai/scibert_scivocab_uncased",
-# "microsoft/BiomedNLP-PubMedBERT-base-uncased-abstract-fulltext", None : to use spacy tokenizer
-transformer_model = "None"
-# transformer_model = "bert-base-uncased"
-# transformer_model = "roberta-base"
-# transformer_model = "allenai/scibert_scivocab_uncased"
-# transformer_model = "microsoft/BiomedNLP-PubMedBERT-base-uncased-abstract-fulltext"
 
 
-# %% Functions
-def generate_output_filename(in_file_path, transformer="None"):
-    # Get the file name including base without the extension
+def generate_output_filename(in_file_path):
     file_name = os.path.splitext(os.path.basename(in_file_path))[0]
     dir_path = os.path.dirname(in_file_path)
-    # Define the output file name
-    if transformer != "None":
-        return os.path.join(dir_path, file_name + "-output-iob-tags-" + transformer + ".tsv")
-    else:
-        return os.path.join(dir_path, file_name + "-output-iob-tags.tsv")
+    return os.path.join(dir_path, file_name + "-output-iob-tags.tsv")
 
 
-# Function to convert a single text and its spans to IOB format
-def convert_to_iob_spacy(lnlp, txt, spns):
+def convert_to_iob(lnlp, txt, spns):
     doc = lnlp(txt)
     iob_tgs = []
 
     for tkn in doc:
-        tg = "O"
-
+        tg = None
         for span in spns:
             if tkn.idx == span["start"]:
                 tg = f"B-{span['label']}"
@@ -49,34 +24,14 @@ def convert_to_iob_spacy(lnlp, txt, spns):
                 tg = f"I-{span['label']}"
                 break
 
+        if not tg:
+            tg = "O"
+
         iob_tgs.append((tkn.text, tg))
 
     return iob_tgs
 
 
-# Function to convert a single text and its spans to IOB format using Hugging Face's tokenizer
-def convert_to_iob_huggingface(tokenizer, txt, spns):
-    tokens = tokenizer.tokenize(txt)
-    token_offsets = tokenizer(txt, return_offsets_mapping=True, truncation=True, padding=False)["offset_mapping"]
-    iob_tgs = ["O"] * len(tokens)
-
-    for span in spns:
-        start, end, label = span["start"], span["end"], span["label"]
-
-        for idx, (start_offset, end_offset) in enumerate(token_offsets):
-            if start_offset == start:
-                iob_tgs[idx] = f"B-{label}"
-                break
-
-        for idx, (start_offset, end_offset) in enumerate(token_offsets):
-            if start_offset > start and end_offset <= end:
-                iob_tgs[idx] = f"I-{label}"
-                break
-
-    return list(zip(tokens, iob_tgs))
-
-
-# Function to convert a list of texts and their spans to IOB format
 def adjust_annotation_spans(new_sentence_start, anns):
     adjusted_annotations = []
     for ann in anns:
@@ -86,7 +41,6 @@ def adjust_annotation_spans(new_sentence_start, anns):
     return adjusted_annotations
 
 
-# Function to split a list of texts and their spans into sentences
 def split_sentences(lnlp, anns, min_length):
     new_anns = []
     for ann in anns:
@@ -107,59 +61,73 @@ def split_sentences(lnlp, anns, min_length):
     return new_anns
 
 
-# Function to load the JSONL annotations file
 def load_annotations(in_file_path):
     with open(in_file_path, "r") as f:
-        # load json objects from jsonl and remove extra whitespaces from the text using textacy
         anns = [json.loads(line) for line in f]
     return anns
 
 
-def convert_to_iob_save(anns, out_file_path, transformer="None") -> None:
-    # Open the output file
+def convert_to_iob_save(lnlp, anns, out_file_path):
+    max_token_length = 0
     with open(out_file_path, "w") as output_file:
-        # Iterate through all the annotations and convert them to IOB format
         for annotation in anns:
             text = annotation["text"]
             spans = annotation["spans"]
-
-            if transformer != "None":
-                if transformer == "bert-base-uncased":
-                    hf_tokenizer = BertTokenizerFast.from_pretrained("bert-base-uncased")
-                elif transformer == "roberta-base":
-                    hf_tokenizer = RobertaTokenizerFast.from_pretrained("roberta-base")
-                elif transformer == "allenai/scibert_scivocab_uncased":
-                    hf_tokenizer = AutoTokenizer.from_pretrained("allenai/scibert_scivocab_uncased")
-                else:
-                    raise ValueError(f"Unsupported transformer model: {transformer}")
-
-                iob_tags = convert_to_iob_huggingface(hf_tokenizer, text, spans)
-            else:
-                iob_tags = convert_to_iob_spacy(nlp, text, spans)
-
-            # Write the IOB tags to the output file
+            iob_tags = convert_to_iob(lnlp, text, spans)
             for token, tag in iob_tags:
                 output_file.write(f"{token}\t{tag}\n")
             output_file.write("\n")
+            if len(iob_tags) > max_token_length:
+                max_token_length = len(iob_tags)
+    return max_token_length
 
+
+def main(file_path, language_model="en_core_web_lg", min_length=None):
+    if not os.path.exists(file_path):
+        print(f"Error: File {file_path} does not exist.")
+        return
+    file_output_path = generate_output_filename(file_path)
+    annotations = load_annotations(file_path)
+    nlp = spacy.load(language_model, disable=["parser", "ner", "textcat"])
+
+    if min_length:
+        if not nlp.has_pipe("sentencizer"):
+            nlp.add_pipe("sentencizer")
+
+        annotations = split_sentences(nlp, annotations, min_length=min_length)
+
+    longest_chunk = convert_to_iob_save(nlp, annotations, file_output_path)
+    print(f"Longest sequence: {longest_chunk}")
+
+
+# # %% Testing
+# # Edit paths here
+# input_file_path = "/home/leroy/Dev/Code/PDPAnnotationDataUtils/annotated_data/tests-mine.jsonl"
+# # in_file = "ciat_ner_v2_combined_rev_20230106.jsonl"
+#
+# # Set the model to use or default to spacy
+# # Choose from "bert-base-uncased", "roberta-base", "allenai/scibert_scivocab_uncased",
+# # "microsoft/BiomedNLP-PubMedBERT-base-uncased-abstract-fulltext", None : to use spacy tokenizer
+# # transformer_model = "None"
+# # transformer_model = "bert-base-uncased"
+# # transformer_model = "roberta-base"
+# # transformer_model = "allenai/scibert_scivocab_uncased"
+# # transformer_model = "microsoft/BiomedNLP-PubMedBERT-base-uncased-abstract-fulltext"
+#
+# main(input_file_path)
+# exit()
 
 # %% Main
 
-# Define the input file path
-file_input_path = os.path.join(file_path, in_file)
-# Define the output file path
-file_output_path = generate_output_filename(file_input_path, transformer_model)
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Convert jsonl annotations to IOB format.')
+    parser.add_argument('-f', '--file_path', required=True, help='Input file name with path.')
+    parser.add_argument('-sm', '--spacy_model', default="en_core_web_lg", help='Spacy language model.')
+    parser.add_argument('-l', '--min_length', default=None, type=int, help='Minimum sentence length, default is the '
+                                                                           'document length')
+    args = parser.parse_args()
 
-annotations = load_annotations(file_input_path)
+    main(args.file_path, args.model, args.min_length)
 
-# Load the English tokenizer from spaCy
-nlp = spacy.load("en_core_web_lg", disable=["parser", "ner", "textcat"])
 
-# Add the "sentencizer" component to the pipeline
-if not nlp.has_pipe("sentencizer"):
-    nlp.add_pipe("sentencizer")
 
-# Process the annotations by splitting the sentences with the specified minimum length
-new_annotations = split_sentences(nlp, annotations, min_length=100)
-
-convert_to_iob_save(new_annotations, file_output_path, transformer_model)
